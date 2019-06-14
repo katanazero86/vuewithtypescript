c5b886e300be1ce6cdbee63c7981e702018dcc3e
kyle <kyle@freshcode.me>
2019-06-14 12:15:07 +0900

    [etc] 컴포넌트 생성

---
+++ src/components/Message.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <input type="text" v-model="message">
+        <div>
+            {{message}}
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    /* script 구문안에 lang 속성을 ts로 지정 */
+    /* 기본적인 vue 컴포넌트 정의 방법 */
+    import {Component, Vue} from 'vue-property-decorator';
+    @Component
+    export default class Message extends Vue {
+        public message: string = '메시지';
+    }
+</script>
+
+<style scoped>
+
+</style>
