811334215fc2597f7d85d2750463ac7d0e73d0e8
kyle <kyle@freshcode.me>
2019-07-09 14:21:40 +0900

    [etc]
    - 컴포넌트 생성
    - props -> @Prop
    - emit -> @Emit
    - watch -> @Watch
    - provide/inject -> @Provide/@Inject
    - model 옵션 -> @Model
    - mixins -> Mixins()
    - event bus
    - Vuex
    - TODO List 예제 작업..

--- src/components/todo/ItemInput.vue
+++ src/components/todo/ItemInput.vue
@@ -7,7 +7,7 @@
 
 <script lang="ts">
 
-    import {Component, Vue} from "vue-property-decorator";
+    import {Component, Vue} from 'vue-property-decorator';
 
     @Component
     export default class ItemInput extends Vue {
